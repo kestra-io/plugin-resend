e7d9e5211bc2ea010807ca5d07d06c6266b2706f
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:57:20 +0100

    chore: update plugin categories (#19)

--- src/main/java/io/kestra/plugin/resend/domain/package-info.java
+++ src/main/java/io/kestra/plugin/resend/domain/package-info.java
@@ -1,6 +1,7 @@
 @PluginSubGroup(
-    description = "Manage and verify domains with the Resend API.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    description = "Manage and verify domains with the Resend API.",categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.resend.domain;
 
--- src/main/java/io/kestra/plugin/resend/email/package-info.java
+++ src/main/java/io/kestra/plugin/resend/email/package-info.java
@@ -1,6 +1,7 @@
 @PluginSubGroup(
-    description = "Send and manage emails using the Resend API.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    description = "Send and manage emails using the Resend API.",categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.resend.email;
 
